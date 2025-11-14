812914aa4d3b47755caff67e2d166ba44d808ab5
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-11-14 14:33:47 +0800

    Add online audio archiving command

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -7,3 +7,4 @@ alias scratchpad="open -a 'iA Writer' '/Users/jon/Library/Mobile Documents/27N4M
 # Archive YouTube
 alias youtube-dl="yt-dlp"
 alias yt-dl="youtube-dl"
+alias yt-audio="youtube-dl --embed-thumbnail --extract-audio --format 'bestaudio[ext=m4a]' -q"
